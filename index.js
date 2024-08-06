0c26c5a0bb4bf6fd7a6ba65030ebd6afa0eaf711
soumya <soumya@example.com>
2024-08-06 11:01:20 +0530

    Ending conflicts

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 console.log("Hello World");
 console.log("Warm welcome to my world");
-<<<<<<< HEAD
-console.log("The life becomes more easy");
-=======
 console.log("Life is going in a right way");
->>>>>>> 466cdfd59831c022f2a57a53e9fbff02cf7ae070
+console.log("It is more happy to share my exp with git");
