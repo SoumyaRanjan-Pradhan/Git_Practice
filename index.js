6f60cde8c7da0eeddabcb5280b8439a6af333977
soumya <soumya@example.com>
2024-08-09 10:16:24 +0530

    total finalized commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 console.log("Hello World");
 console.log("Warm welcome to my world");
 console.log("Life is going in a right way");
-<<<<<<< HEAD
-console.log("This is amazing exp with git");
-=======
 console.log("This is nice exp with git and github");
->>>>>>> 39a5d2dc5e68fd4918523c12a7579f630a26d3ef
+const rough = ()=>{
+    
+}
