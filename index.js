8f33af385cdd6611939db59a35dae1010745cd0b
soumya <soumya@example.com>
2024-08-09 10:37:50 +0530

    stash first commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hii");
+console.log("Hello World");
+console.log("Warm welcome to my world");
+console.log("Life is going in a right way");
+console.log("This is nice exp with git and github");
+const rough = ()=>{
+  console.log("Ram is a good boy");  
+}
