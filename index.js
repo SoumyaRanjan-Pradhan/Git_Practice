0187b988e935c9a64db4b1b2acff4d82c114260f
soumya <soumya@example.com>
2024-08-14 11:49:55 +0530

    add index.js into gitignore file

--- index.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World");
-console.log("Warm welcome to my world");
-console.log("Life is going in a right way");
-console.log("This is nice exp with git and github");
-const rough = ()=>{
-    
-}
